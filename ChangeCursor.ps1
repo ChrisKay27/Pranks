d36a28caf5cf034b961063b875384d4774a6e377
ChrisKay27 <chris.kaebe27@hotmail.com>
2017-04-24 10:39:55 -0700

    ChangeCursor.ps1

---
+++ ChangeCursor.ps1
@@ -0,0 +1,26 @@
+$RegConnect = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]"CurrentUser","$env:COMPUTERNAME")
+$RegCursors = $RegConnect.OpenSubKey("Control Panel\Cursors",$true)
+$RegCursors.SetValue("Arrow","C:\Windows\cursors\aero_busy.ani")
+$RegCursors.Close()
+
+$RegConnect.Close()
+
+$CSharpSig = @'
+
+[DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+
+public static extern bool SystemParametersInfo(
+
+                 uint uiAction,
+
+                 uint uiParam,
+
+                 uint pvParam,
+
+                 uint fWinIni);
+
+'@
+
+$CursorRefresh = Add-Type -MemberDefinition $CSharpSig -Name WinAPICall -Namespace SystemParamInfo -PassThru
+
+$CursorRefresh::SystemParametersInfo(0x0057,0,$null,0)
